5b756d4903c920ce1cabbc96bfebd2301ca3e52d
raydalessandro <alessio.marrone1993@gmail.com>
2025-11-15 00:06:39 +0100

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-white text-center py-6 mt-10">
+      <p>© 2025 Vodafone Brianza - Tutti i diritti riservati</p>
+      <p className="text-sm">Contatti: info@vodafonebrianza.it | Tel: +39 02 123456</p>
+    </footer>
+  )
+}
